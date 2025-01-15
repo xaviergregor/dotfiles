aa3c43082625f7ce34c5fd7f4490d3397a3fb0c4
xaviergregor <xavier@gregor.fr>
2025-01-15 22:27:11 +0100

    remove old script

--- linux/removepopup_proxmox.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-# Patch du fichier JS
-sed -i.bak "s/.data.status.toLowerCase() !== 'active') {/.data.status.toLowerCase() !== 'active') { orig_cmd\(\); } else if ( false ) {/" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js
-# Restart PVE Web
-systemctl restart pveproxy.service
-# Restart PBS Web
-#systemctl restart proxmox-backup-proxy.service
